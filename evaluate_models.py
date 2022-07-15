8b6637bac5b32238f8f010f6c961c77e48d1ab9d
Tiago Oliveira <tro@tejo.mlkd.tp.vps.inesc-id.pt>
2022-07-15 15:20:53 +0100

    it's working for some tests

--- evaluate_models.py
+++ evaluate_models.py
@@ -15,7 +15,7 @@ use datafra,e
 
 """
 
-from nbformat import write
+#from nbformat import write
 from decision_mappings import ImageNetProbabilitiesTo1000ClassNamesMapping, ImageNetProbabilitiesTo16ClassNamesMapping
 from helper import wordnet_functions as wnf
 
@@ -30,75 +30,87 @@ from cue_conflict_dataset import CueConflictDataloader
 import csv
 from tqdm import tqdm
 from pl_bolts.models.self_supervised import SimCLR
+import os
 
 
-""" 
-EVALUATION
-"""
-#dataset = ImageFolder("./cue-conflict",transform=transforms.ToTensor())
 
-#model = models.resnet50(pretrained=True)
-#model.fc = SimCLR(0,1,1,None).encoder.fc
-checkpoint = torch.load("./checkpoint_100.tar",map_location=torch.device('cpu'))
-model = models.resnet50().load_state_dict(checkpoint)
+class ShapeBiasEvaluator:
+    def __init__(self,dataset_path,resize:bool=True,batch_size=128,workers=4,device=None):
+        self.dataset_path = dataset_path
+        self.resize = resize
+        self.batch_size = batch_size
+        self.workers = workers
+        self.dataloader = CueConflictDataloader()("cue-conflict",True,self.batch_size,self.workers)
+        if device is None:
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        else:
+            self.device = device
+
+    def _run(self,model,filename="results.csv",max_lines=None):
+        max_lines = max_lines if max_lines is not None else -1        
+        mapp = ImageNetProbabilitiesTo16ClassNamesMapping()
+        with open(filename,"w") as fp:
+            writer = csv.writer(fp)
+            writer.writerow(["response", "shape","texture", "imagename"])
+            for x,shapes,textures,paths in tqdm(self.dataloader):
+                with torch.no_grad():
+                    x = x.to(self.device)
+                    output = torch.nn.functional.softmax(model(x),dim=1)
+                    classes_batch = mapp(output.cpu().numpy())
+                    for classes,shape,texture,path in zip(classes_batch,shapes,textures,paths):
+                        writer.writerow([classes[0],shape,texture,path])
+                        max_lines -= 1
+                        if max_lines == 0: break
+                if max_lines == 0: break
+
+    def _summarize(self,filename="results.csv",verbose=True):
+        results = {}
+        total,shape_or_texture, shape, texture = 0,0,0,0
+        with open(filename,"r") as fp:
+            reader = csv.reader(fp)
+            reader.__next__() #dump first line
+            for row in reader:
+                if row[0] == row[1]:
+                    shape_or_texture += 1
+                    shape += 1
+                elif row[0] == row[2]:
+                    shape_or_texture += 1
+                    texture += 1
+                total += 1
+            results = {"total":total,
+                    "shape_bias":shape/shape_or_texture,
+                    "shape_match":shape/total,
+                    "texture_match":texture/total}
+            info = ""
+            info += "total: " + str(results["total"]) + "\n"
+            info += "shape_bias: " + str(results["shape_bias"]) + "\n"
+            info += "shape_match: " + str(results["shape_match"]) + "\n"
+            info += "texture_match: " + str(results["texture_match"]) + "\n"
+            if verbose: print(info)
+            return results, info
+
+    def evaluate(self,model,filename="results.csv",max_lines=None,verbose=True,keep_result_file=False):
+        model.to(self.device)
+        self._run(model,filename=filename)
+        results, info = self._summarize(filename,verbose)
+        assert os.path.exists(filename)
+        os.remove(filename)
+        return results, info
+
+    
 
 
-#simple_mapp = ImageNetProbabilitiesTo1000ClassNamesMapping()
-dataloader = CueConflictDataloader()("cue-conflict",True,10,4)
-file_name = "results.csv"
-max_lines = -1
 
-def run(dataloader,model,filename="results.csv",max_lines=None):
-    if max_lines == None: max_lines = -1
-    mapp = ImageNetProbabilitiesTo16ClassNamesMapping()
-    with open(filename,"w") as fp:
-        writer = csv.writer(fp)
-        writer.writerow(["response", "shape","texture", "imagename"])
-        
-        for x,shapes,textures,paths in tqdm(dataloader):
-            with torch.no_grad():
-                classes_batch = mapp(torch.nn.functional.softmax(model(x),dim=1).numpy())
-                for classes,shape,texture,path in zip(classes_batch,shapes,textures,paths):
-                    writer.writerow([classes[0],shape,texture,path])
-                    max_lines -= 1
-                    if max_lines == 0: break
-            if max_lines == 0: break
-
-
-""" 
-PLOT
-"""
-def plot(filename="results.csv",verbose=True):
-    total,shape_or_texture, shape, texture = 0,0,0,0
-    with open(filename,"r") as fp:
-        reader = csv.reader(fp)
-        reader.__next__() #dump first line
-        for row in reader:
-            if row[0] == row[1]:
-                shape_or_texture += 1
-                shape += 1
-            elif row[0] == row[2]:
-                shape_or_texture += 1
-                texture += 1
-            total += 1
-        info = ""
-        info += "shape: " + str(shape) + "\n"
-        info += "texture: " + str(texture) + "\n"
-        info += "shape or texture: " + str(shape_or_texture) + "\n"
-        info += "total: " + str(total) + "\n"
-        info += "shape%: " + str(shape/shape_or_texture) + "\n"
-        info += "texture%: " + str(texture/shape_or_texture) + "\n"
-        if verbose: print(info)
-        return info
-
-
-def evaluate(dataloader,model,filename="results.csv",verbose=True):
-    run(dataloader,model,filename=filename)
-    info = plot(filename,verbose)
-    return info
-
-
-evaluate(dataloader,model)
+if __name__ == '__main__':
+    import timm
+    #model = models.resnet18(pretrained=True)
+    model = timm.create_model('resnet18d',pretrained=True)
+    dataloader = CueConflictDataloader()("cue-conflict",True,128,4)
+    file_name = "results.csv"
+    max_lines = -1
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    verbose = True
+    ShapeBiasEvaluator("cue-conflict").evaluate(model)
 
 
 
--- train.py
+++ train.py
@@ -1,163 +1,306 @@
-"""Fine-tuning last layer models"""
-
-
+""" Fine-tuning last layer models """
 from torchvision import datasets, transforms as T, models
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from albumentations.augmentations import ColorJitter, Blur, GaussNoise, GaussianBlur
+from torch.utils.data import Dataset
+
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.loggers import TensorBoardLogger
 import torch
 from torch.nn import functional as F
-import sys
 from os.path import abspath, dirname
 from torch.utils.data import random_split, DataLoader
 
 """ Imports from torch core """
+import sys
 sys.path.insert(0, "/home/guests2/tro/torch-core")
 from imagenet_dataset import ImageNetDataset
 from auxiliary import get_accuracy, get_crossentropy
 from imagenet_light import Model as Good_Model
 
+""" Imports from this directory """
+from evaluate_models import ShapeBiasEvaluator
+from cue_conflict_dataset import CueConflictDataloader
 
 
+""" Helper class """
+class Transform_Dataset(Dataset):
+    """
+    This is so that we can take a dataset and append it a transformation.
+    It is particularly useful when we want to use data augmentations to train that 
+    are different from those to validate.
+    """
+    def __init__(self,dataset,transform=None):
+        self.dataset = dataset
+        self.transform = transform
 
+    def __len__(self):
+        return len(self.dataset)
+        
+    def __getitem__(self, idx):
+        image,label = self.dataset[idx]
+        if self.transform:
+            if isinstance(self.transform,A.Compose): #Albumentations librar
+                augmented = self.transform(image=image)
+                image = augmented['image']
+            else: 
+                image = self.transform(image)
+        return image, label
+
+
+""" Helper functions """
 def freeze_all(model):
-		for params in model.parameters():
-				params.requires_grad = False
-		return model
+        for params in model.parameters():
+                params.requires_grad = False
+        return model
 
 
 def freeze_all_but_fc(model):
-		for params in model.parameters():
-				params.requires_grad = False
-		for params in model.fc.parameters():
-				params.requires_grad = True
-		return model
+        for params in model.parameters():
+                params.requires_grad = False
+        for params in model.fc.parameters(): #!
+                params.requires_grad = True
+        return model
 
 def unfreeze_all(model):
-		for params in model.parameters():
-				params.requres_grad = True
-		return model     
+        for params in model.parameters():
+                params.requres_grad = True
+        return model     
 
 def reset_fc(model):
-		model.fc = type(model.fc)(model.fc.in_features,model.fc.out_features)
-		for params in model.fc.parameters():
-				assert params.requires_grad
-		return model
-				
+        #model.fc = type(model.fc)(model.fc.in_features,model.fc.out_features)
+        model.fc = type(model.fc)(model.fc.in_features,model.fc.out_features)
+        for params in model.fc.parameters():
+                assert params.requires_grad
+        return model
+                
 def check_if_frozen_but_fc(model):
-	#except last
-	for params in list(model.parameters())[:-2]:
-			assert not params.requires_grad
+    #except last
+    for params in list(model.parameters())[:-2]:
+            assert not params.requires_grad
+
+def unormalize(batch,device):
+    x = batch
+
+    mean1 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.485
+    mean2 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.456
+    mean3 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.406
+    mean = torch.concat((mean1,mean2,mean3),dim=0) * 255.0
+
+
+    std1 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.229
+    std2 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.224
+    std3 = torch.ones(1,x[0].shape[1],x[0].shape[2]) * 0.225
+    std = torch.concat((std1,std2,std3),dim=0) * 255.0
+
+    new_x = x[0] * std.to(device) + mean.to(device)
+    new_x = new_x.unsqueeze(0)
+
+    return new_x
 
 
 
+""" Model """
+
+class Modelo(pl.LightningModule):
+    def __init__(self,pretrained=True): 
+            super().__init__()
+            self.model = models.resnet50(pretrained=pretrained)    
+    
+    def forward(self,x):
+            embedding = self.model(x)
+            return embedding
+
+    def configure_optimizers(self):
+            #self.optimizer = torch.optim.Adam(self.parameters(), lr=3e-4,weight_decay=1e-4)
+            self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+            return self.optimizer
+
+        
+    def training_step(self,train_batch,batch_idx):
+            assert self.model.training
+            #check_if_frozen_but_fc(self.model)
+            x, y = train_batch
+            x = x.float()
+            z = self.model(x)    
+            loss = F.cross_entropy(z, y)
+            self.log('train_loss', loss)
 
 
+            #self.experiment.image(0, x)
+            tensorboard = self.logger.experiment
+            x = unormalize(x,torch.device("cuda"))
+            tensorboard.add_images('new_images',x.to(torch.uint8),batch_idx)
 
+            #tensorboard.add_images('images',x[0].to(torch.uint8).unsqueeze(0),batch_idx)
+            #tensorboard.add_images('new_images',new_x.to(torch.uint8),batch_idx)
+            #print(x[0].dtype)
+            #print(type(tensorboard))
+            return loss
 
+    def validation_step(self,val_batch,batch_idx):
+            assert not self.model.training
+            #check_if_frozen_but_fc(self.model)
+            x, y = val_batch
+            z = self.model(x)
+            loss = F.cross_entropy(z, y)
+            self.log('val_loss', loss)
+     
+    def freeze_all_but_fc(self):
+        freeze_all_but_fc(self.model)
+            
+    def unfreeze(self):
+        unfreeze_all(self.model)
 
-class Resnet18(pl.LightningModule):
-	def __init__(self,pretrained=True):
-			super().__init__()
-			self.model = models.resnet18(pretrained=pretrained)	
-	
-	def forward(self,x):
-			embedding = self.model(x)
-			return embedding
+    def reset_fc(self):
+        reset_fc(self.model)
+    
 
-	def configure_optimizers(self):
-			self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-			return self.optimizer
-
-		
-	def training_step(self,train_batch,batch_idx):
-			assert self.model.training
-			#check_if_frozen_but_fc(self.model)
-			x, y = train_batch
-			z = self.model(x)    
-			loss = F.cross_entropy(z, y)
-			self.log('train_loss', loss)
-			return loss
-
-
-	def validation_step(self,val_batch,batch_idx):
-			assert not self.model.training
-			#check_if_frozen_but_fc(self.model)
-			x, y = val_batch
-			z = self.model(x)
-			loss = F.cross_entropy(z, y)
-			self.log('val_loss', loss)
-		
-
-	def freeze_all_but_fc(self):
-			freeze_all_but_fc(self.model)
-			
-	def unfreeze(self):
-			unfreeze_all(self.model)
-
-	def reset_fc(self):
-		reset_fc(self.model)
-	
-
-
+""" Callback/Hook for Torch Lightning """
 class MyEvaluation(Callback):
-	def __init__(self,dataset):
-		super().__init__()
-		self.dataset = dataset
+    def __init__(self,val_dataset,train_dataset):
+        super().__init__()
+        self.val_dataset = val_dataset
+        self.train_dataset = train_dataset
+        self.shapebias_evaluator = ShapeBiasEvaluator("cue-conflict")
 
-	def on_validation_epoch_start(self, trainer, model):
-		acc = get_accuracy(model,self.dataset,100)
-		model.log('100-accuracy',acc)
-		print("Validating Externally " + str(acc))
+    def on_validation_epoch_start(self, trainer, model):
+        print("calculating accuracies... ")
+        assert not model.training
+        val_acc = get_accuracy(model,self.val_dataset,10)
+        train_acc = get_accuracy(model,self.train_dataset,10)
+        results, info = self.shapebias_evaluator.evaluate(model,verbose=False)
+        model.log('val-accuracy',val_acc)
+        model.log('1000-train_accuracy',train_acc)
+        model.log('shape-bias',results["shape_bias"])
+        model.log('shape-match',results["shape_match"])
+        model.log('texture-match',results["texture_match"])
+        print("\nAccuracy Validation: " + str(val_acc))
+        print("Accuracy Training: " + str(train_acc))
+        print(info)
 
-	
-	""" 
-	Just to test
-	def on_train_batch_start(self,trainer,model,batch,batch_idx):
-		assert model.training
-		model.eval()
-		assert not model.training
-		acc = get_accuracy(model,self.dataset,100)
-		model.log('100-accuracy',acc)
-		print("batch "+str(batch_idx) + " Externally " + str(acc))
-		model.train()
-		assert model.training
-	"""
+    
 
-
-
+""" Main """
 def main():
-	""" Initializations """
-	model = Resnet18(True)
-	standard_transform = T.Compose([
-											T.ToTensor(),
-											T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]), 
-											T.Resize((224,224))]) 
-	dataset = ImageNetDataset(transform=standard_transform)
-	print(len(dataset))
-			
+    """ Initializations """
+    model = Modelo()
+
+    """ Transformations """
+    null_transform = A.Compose([A.Resize(224, 224),
+                                ToTensorV2()])
+
+    standard_transform = T.Compose([
+                            T.ToTensor(),
+                            T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]), 
+                            T.Resize((224,224))]) 
+
+    standard_transform = A.Compose([
+                                    A.Normalize(
+                                        mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225],
+                                    ),
+                                    A.Resize(224, 224), 
+                                    ToTensorV2()
+                                ])
+
+
+    special_transform = A.Compose([
+                                A.Normalize(
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225],
+                                ),
+                                A.Resize(224, 224),
+                                A.Compose([
+                                        ColorJitter(p=1),
+                                        GaussianBlur(p=1),
+                                        GaussNoise(p=1),
+                                ],p=0.5),
+                                ToTensorV2()
+    ])
+
+    gaussnoise_transform = A.Compose([
+                                A.Normalize(
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225],
+                                ),
+                                A.Resize(224, 224),
+                                GaussNoise(var_limit=(0.0,0.005),p=1), #!
+                                ToTensorV2()
+    ])
+
+
+    color_jitter_transform = A.Compose([
+                                A.Normalize(
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225],
+                                ),
+                                A.Resize(224, 224),
+                                ColorJitter(p=1), #!
+                                ToTensorV2()
+    ])
 
 
 
-	""" Prepare model and check its accuracy"""
-	model.reset_fc()
-	model.freeze_all_but_fc()
-	model.eval()
-	acc = get_accuracy(model,dataset,100)
-	print("accuracy:"+str(acc)) #should be pretty low, if not 0.0
-	assert acc < 0.10
+
+    
+
+    print("using special transform")
+    dataset = ImageNetDataset()
+    print(len(dataset))
+            
+
+    """ Prepare model and check its accuracy"""
+    model.reset_fc() 
+    model.freeze_all_but_fc() 
+    model.eval()
+    #acc = get_accuracy(model,Transform_Dataset(dataset,transform=standard_transform),1000) #!
+    acc = get_accuracy(model,Transform_Dataset(dataset,transform=standard_transform),10)
+    print("accuracy:"+str(acc)) #should be pretty low, if not 0.0
+    assert acc < 0.10
 
 
-	""" Train"""
-	total_data_percentage, train_val_percentage = 0.4, 0.8
-	split_dataset, garbage = random_split(dataset, [int(len(dataset)*total_data_percentage), len(dataset)-int(len(dataset)*total_data_percentage)])
-	train_data, val_data = random_split(split_dataset, [int(len(split_dataset)*train_val_percentage), len(split_dataset)-int(len(split_dataset)*train_val_percentage)])
-	print("size of dataset used: "+str(len(train_data)))
-	train_loader = DataLoader(train_data, batch_size=128,num_workers=10,persistent_workers=True) #!
-	val_loader = DataLoader(val_data, batch_size=128,num_workers=10,persistent_workers=True) #!
+    """ Train"""
+    total_data_percentage, train_val_percentage = 0.2, 0.9
+    split_dataset, garbage = random_split(dataset, [int(len(dataset)*total_data_percentage), len(dataset)-int(len(dataset)*total_data_percentage)])
 
-	trainer = pl.Trainer(gpus=[0],callbacks=[MyEvaluation(dataset)])
-	trainer.fit(model, train_loader, val_loader)
+    train_data, val_data = random_split(split_dataset, [int(len(split_dataset)*train_val_percentage), len(split_dataset)-int(len(split_dataset)*train_val_percentage)])
+    
+    train_data = Transform_Dataset(train_data,transform=gaussnoise_transform) #!
+    val_data = Transform_Dataset(val_data,transform=standard_transform)
+
+    print("size of train dataset used: "+str(len(train_data)))
+    print("size of val dataset used: "+str(len(val_data)))
+    #train_loader = DataLoader(train_data, batch_size=64,num_workers=10,persistent_workers=True) #!
+    #val_loader = DataLoader(val_data, batch_size=64,num_workers=10,persistent_workers=True) #!
+    train_loader = DataLoader(train_data, batch_size=64,num_workers=10,persistent_workers=True,shuffle=True) #!
+    val_loader = DataLoader(val_data, batch_size=64,num_workers=10,persistent_workers=True,shuffle=False) #!
+
+    #logger = TensorBoardLogger('logs', name='test')
+    trainer = pl.Trainer(gpus=[0],callbacks=[MyEvaluation(val_data,train_data)])
+    trainer.fit(model, train_loader, val_loader)
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
+
+ #gaussian blur 0.5 Default resnet18 ZEZERE --- killed not work
+ 
+ #gaussian blur 1  Default resnet18 ZEZERE --- kileed not work
+
+ #gaussian blur 0 Default resnet18 TEJO -- killed works well
+ 
+ #gaussian blur 1 (0,2) resnet18 Zezere -- killed not work
+
+ #gaussian blur 1 (0,0.5) resnet18 Zezere -- killed not work
+
+ #gaussian blur 1 (0,0.05) resnet18 Zezere 
+
+ #color jitter Default resnet18 Tejo -- killed not work
+
+ #gaussian blur 1 (0,0.005) resnet18 Zezere
+
+ #gaussian blur 1 (0,0.005) resnet50 tejo
